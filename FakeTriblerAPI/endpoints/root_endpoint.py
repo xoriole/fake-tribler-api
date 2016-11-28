c46dd69bbc67abddf943b037d4617050609a4032
Martijn de Vos <martijn@code-up.nl>
2016-11-28 11:52:08 +0100

    Added shutdown endpoint

--- FakeTriblerAPI/endpoints/root_endpoint.py
+++ FakeTriblerAPI/endpoints/root_endpoint.py
@@ -6,6 +6,7 @@ from FakeTriblerAPI.endpoints.events_endpoint import EventsEndpoint
 from FakeTriblerAPI.endpoints.multichain_endpoint import MultichainEndpoint
 from FakeTriblerAPI.endpoints.mychannel_endpoint import MyChannelEndpoint
 from FakeTriblerAPI.endpoints.search_endpoint import SearchEndpoint
+from FakeTriblerAPI.endpoints.shutdown_endpoint import ShutdownEndpoint
 from FakeTriblerAPI.endpoints.state_endpoint import StateEndpoint
 from FakeTriblerAPI.endpoints.statistics_endpoint import StatisticsEndpoint
 from FakeTriblerAPI.endpoints.torrentinfo_endpoint import TorrentInfoEndpoint
@@ -29,7 +30,8 @@ class RootEndpoint(resource.Resource):
                               "settings": SettingsEndpoint, "variables": VariablesEndpoint,
                               "downloads": DownloadsEndpoint, "torrents": TorrentsEndpoint,
                               "multichain": MultichainEndpoint, "statistics": StatisticsEndpoint,
-                              "state": StateEndpoint, "torrentinfo": TorrentInfoEndpoint}
+                              "state": StateEndpoint, "torrentinfo": TorrentInfoEndpoint,
+                              "shutdown": ShutdownEndpoint}
 
         for path, child_cls in child_handler_dict.iteritems():
             self.putChild(path, child_cls())

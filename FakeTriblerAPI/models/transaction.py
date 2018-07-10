d73d71a8e1e05e587d3e6e50333a88ffa5f1f43c
Martijn de Vos <mdmartijndevos@gmail.com>
2018-07-10 15:28:20 +0200

    Updated FakeTriblerAPI endpoints

--- FakeTriblerAPI/models/transaction.py
+++ FakeTriblerAPI/models/transaction.py
@@ -2,6 +2,8 @@ import random
 
 import time
 
+from FakeTriblerAPI.models.payment import Payment
+
 
 class Transaction:
 
@@ -11,16 +13,17 @@ class Transaction:
         self.order_number = random.randint(1, 50)
         self.partner_order_number = random.randint(1, 50)
         self.transaction_number = random.randint(1, 50)
-        self.price = random.randint(1, 100)
-        self.price_type = price_type
-        self.transferred_price = random.randint(0, self.price)
-        self.quantity = random.randint(1, 100)
-        self.quantity_type = quantity_type
-        self.transferred_quantity = random.randint(0, self.quantity)
+        self.asset1_amount = random.randint(1, 100)
+        self.asset1_type = price_type
+        self.transferred_asset1 = random.randint(0, self.asset1_amount)
+        self.asset2_amount = random.randint(1, 100)
+        self.asset2_type = quantity_type
+        self.transferred_asset2 = random.randint(0, self.asset2_amount)
         now = int(time.time())
         self.timestamp = random.randint(now - 3600, now)
         self.payment_complete = random.random() > 0.5
         self.status = random.choice(['pending', 'completed', 'error'])
+        self.payments = [Payment(self)]
 
     def get_json(self):
         return {
@@ -29,12 +32,26 @@ class Transaction:
             "partner_trader_id": self.partner_trader_id,
             "partner_order_number": self.partner_order_number,
             "transaction_number": self.transaction_number,
-            "price": self.price,
-            "price_type": self.price_type,
-            "transferred_price": self.transferred_price,
-            "quantity": self.quantity,
-            "quantity_type": self.quantity_type,
-            "transferred_quantity": self.transferred_quantity,
+            "assets": {
+                "first": {
+                    "amount": self.asset1_amount,
+                    "type": self.asset1_type
+                },
+                "second": {
+                    "amount": self.asset2_amount,
+                    "type": self.asset2_type
+                }
+            },
+            "transferred": {
+                "first": {
+                    "amount": self.transferred_asset1,
+                    "type": self.asset1_type
+                },
+                "second": {
+                    "amount": self.transferred_asset1,
+                    "type": self.asset2_type
+                }
+            },
             "timestamp": self.timestamp,
             "payment_complete": self.payment_complete,
             "status": self.status
